cb6433185cc226e1740cef0871194ceb4de16aee
Achintya Ashok <aa3099@nyu.edu>
2016-10-05 23:44:29 -0400

    modularizing the library

---
+++ index.js
@@ -0,0 +1,7 @@
+var path = require('path');
+var kayak = require(path.join(__dirname, "lib/kayakScraper.js"));
+
+// export whatever the kayak library exports
+module.exports = {
+  kayak: kayak
+};
