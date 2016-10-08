7d01b3b68eaa20ae634a10afd269685f34d46d51
Achintya Ashok <aa3099@nyu.edu>
2016-10-08 15:13:35 -0400

    added tests for month and single digit month

--- spec/kayakScraper-spec.js
+++ spec/kayakScraper-spec.js
@@ -1,11 +1,16 @@
+var path = require('path');
 var kayak = require(path.join(__dirname, '../lib/kayakScraper'));
 
 describe('KayakScraper',function(){
   /* Test the formatted date function */
   describe('formattedDate', function(){
-    it('should return 2016-10-01 for month greater than 10', function(){
-      var formattedDate = kayak.getFormattedDate(new Date(2016, 10, 1));
-      expect(formattedDate).toBe('2016-10-01');
+    it('should return 2016-10-01 for October 10th, 2016', function(){
+      var formattedDate = kayak.getFormattedDate(new Date(2016, 9, 16)); // months are 0-indexed
+      expect(formattedDate).toBe('2016-10-16');
+    });
+    it('should return zero-prefix for single digit month', function(){
+      var formattedDate = kayak.getFormattedDate(new Date(2016, 8, 16));
+      expect(formattedDate).toBe('2016-09-16');
     });
   })
 });
